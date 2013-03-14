9fdef57db191ac23831c3821ebf2e506057845de
Siniša <Benediktus>
2013-03-14 14:11:58 +0100

    Migration to VS2012

--- VegaRpgGamePrototypes/VegaRpgWebApi/Properties/AssemblyInfo.cs
+++ VegaRpgGamePrototypes/VegaRpgWebApi/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0dfe1367-8d28-494a-acc3-98bd19086d33")]
+[assembly: Guid("2d929b58-2205-4bb9-8366-d1c03cfb85fc")]
 
 // Version information for an assembly consists of the following four values:
 //
